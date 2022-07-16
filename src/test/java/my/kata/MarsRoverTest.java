ffc5f9e391d6d8b6c7debf7c5a89cfc406087abd
wubin28 <wubinben@icloud.com>
2022-07-16 20:40:08 +0800

    test: add should_go_to_1_0_E_when_move_given_from_0_0_E()

--- src/test/java/my/kata/MarsRoverTest.java
+++ src/test/java/my/kata/MarsRoverTest.java
@@ -88,4 +88,18 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getYCoordinate());
         assertEquals("W", marsRover.getDirection());
     }
+
+    @Test
+    void should_go_to_1_0_E_when_move_given_from_0_0_E() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, "E");
+
+        // when
+        marsRover.performStoredCommands("M");
+
+        // then
+        assertEquals(1, marsRover.getXCoordinate());
+        assertEquals(0, marsRover.getYCoordinate());
+        assertEquals("E", marsRover.getDirection());
+    }
 }
