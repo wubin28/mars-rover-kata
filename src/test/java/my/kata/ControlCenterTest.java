fd6c6fa67937a8e477cb4d8a93bde23ebf4fc15c
wubin28 <wubinben@icloud.com>
2022-07-16 23:24:21 +0800

    test: add ControlCenterTest.should_go_to_minus_1_0_W_when_move_given_from_0_0_W()

--- src/test/java/my/kata/ControlCenterTest.java
+++ src/test/java/my/kata/ControlCenterTest.java
@@ -162,4 +162,21 @@ public class ControlCenterTest {
         assertEquals(0, marsRover.getYCoordinate());
         assertEquals("S", marsRover.getDirection());
     }
+
+    @Test
+    void should_go_to_minus_1_0_W_when_move_given_from_0_0_W() {
+        // given
+        ControlCenter controlCenter = new ControlCenter();
+        MarsRover marsRover = new MarsRover(0, 0, "W");
+        MoveCommand moveCommand = new MoveCommand(marsRover);
+        controlCenter.storeCommands(moveCommand);
+
+        // when
+        controlCenter.performStoredCommands();
+
+        // then
+        assertEquals(-1, marsRover.getXCoordinate());
+        assertEquals(0, marsRover.getYCoordinate());
+        assertEquals("W", marsRover.getDirection());
+    }
 }
