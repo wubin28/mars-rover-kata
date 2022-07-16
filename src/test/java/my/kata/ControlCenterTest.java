6b2980e94d91318d1c87dd302ecd67de0fd1b0b6
wubin28 <wubinben@icloud.com>
2022-07-16 23:27:35 +0800

    test: add ControlCenterTest.should_go_to_0_0_S_when_turn_left_given_from_0_0_W()

--- src/test/java/my/kata/ControlCenterTest.java
+++ src/test/java/my/kata/ControlCenterTest.java
@@ -179,4 +179,21 @@ public class ControlCenterTest {
         assertEquals(0, marsRover.getYCoordinate());
         assertEquals("W", marsRover.getDirection());
     }
+
+    @Test
+    void should_go_to_0_0_S_when_turn_left_given_from_0_0_W() {
+        // given
+        ControlCenter controlCenter = new ControlCenter();
+        MarsRover marsRover = new MarsRover(0, 0, "W");
+        TurnLeftCommand turnLeftCommand = new TurnLeftCommand(marsRover);
+        controlCenter.storeCommands(turnLeftCommand);
+
+        // when
+        controlCenter.performStoredCommands();
+
+        // then
+        assertEquals(0, marsRover.getXCoordinate());
+        assertEquals(0, marsRover.getYCoordinate());
+        assertEquals("S", marsRover.getDirection());
+    }
 }
