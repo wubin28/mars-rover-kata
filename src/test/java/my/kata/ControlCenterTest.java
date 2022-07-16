660cda76d1ce3cd733c25cb2ac39016d352d3677
wubin28 <wubinben@icloud.com>
2022-07-16 22:17:39 +0800

    test: add ControlCenterTest.should_go_to_0_1_N_when_move_given_from_0_0_N()

---
+++ src/test/java/my/kata/ControlCenterTest.java
@@ -0,0 +1,24 @@
+package my.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ControlCenterTest {
+    @Test
+    void should_go_to_0_1_N_when_move_given_from_0_0_N() {
+        // given
+        ControlCenter controlCenter = new ControlCenter();
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+        MoveCommand moveCommand = new MoveCommand(marsRover);
+        controlCenter.storeCommands(moveCommand);
+
+        // when
+        controlCenter.performStoredCommands();
+
+        // then
+        assertEquals(0, marsRover.getXCoordinate());
+        assertEquals(1, marsRover.getYCoordinate());
+        assertEquals("N", marsRover.getDirection());
+    }
+}
