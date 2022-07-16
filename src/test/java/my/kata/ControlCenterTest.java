ea2eae362f9c58f7831ea5910682b549d9d1d905
wubin28 <wubinben@icloud.com>
2022-07-16 23:15:02 +0800

    test: add ControlCenterTest.should_go_to_1_0_E_when_move_given_from_0_0_E()

--- src/test/java/my/kata/ControlCenterTest.java
+++ src/test/java/my/kata/ControlCenterTest.java
@@ -111,4 +111,21 @@ public class ControlCenterTest {
         assertEquals(0, marsRover.getYCoordinate());
         assertEquals("W", marsRover.getDirection());
     }
+
+    @Test
+    void should_go_to_1_0_E_when_move_given_from_0_0_E() {
+        // given
+        ControlCenter controlCenter = new ControlCenter();
+        MarsRover marsRover = new MarsRover(0, 0, "E");
+        MoveCommand moveCommand = new MoveCommand(marsRover);
+        controlCenter.storeCommands(moveCommand);
+
+        // when
+        controlCenter.performStoredCommands();
+
+        // then
+        assertEquals(1, marsRover.getXCoordinate());
+        assertEquals(0, marsRover.getYCoordinate());
+        assertEquals("E", marsRover.getDirection());
+    }
 }
