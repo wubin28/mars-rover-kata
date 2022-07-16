a7c92b3b874aac86253fb379f259e152c5a166c6
wubin28 <wubinben@icloud.com>
2022-07-16 19:16:19 +0800

    feat: should_go_to_0_1_N_when_move_given_from_0_0_N

---
+++ src/main/java/my/kata/MarsRover.java
@@ -0,0 +1,23 @@
+package my.kata;
+
+public class MarsRover {
+    public MarsRover(int xCoordinate, int yCoordinate, String direction) {
+
+    }
+
+    public void performStoredCommands(String direction) {
+
+    }
+
+    public int getXCoordinate() {
+        return 0;
+    }
+
+    public int getYCoordinate() {
+        return 1;
+    }
+
+    public String getDirection() {
+        return "N";
+    }
+}
