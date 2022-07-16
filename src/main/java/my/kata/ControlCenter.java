92e17a75d6341f33fa5a37e21c4ebf7a4607ca0c
wubin28 <wubinben@icloud.com>
2022-07-16 23:34:37 +0800

    feat: use varargs in ControlCenter.storeCommands()

--- src/main/java/my/kata/ControlCenter.java
+++ src/main/java/my/kata/ControlCenter.java
@@ -1,13 +1,15 @@
 package my.kata;
 
 public class ControlCenter {
-    private Command command;
+    private Command[] commands;
 
-    public void storeCommands(Command command) {
-        this.command = command;
+    public void storeCommands(Command ... commands) {
+        this.commands = commands;
     }
 
     public void performStoredCommands() {
-        this.command.execute();
+        for (Command command : commands) {
+            command.execute();
+        }
     }
 }
