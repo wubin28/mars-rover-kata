4f203b607f7c778925bc1fc50d31f64895cdf3ff
wubin28 <wubinben@icloud.com>
2022-07-16 11:12:00 +0800

    baseline code

---
+++ src/main/java/my/kata/Application.java
@@ -0,0 +1,4 @@
+package my.kata;
+
+public class Application {
+}
---
+++ src/test/java/my/kata/DemoTest.java
@@ -0,0 +1,10 @@
+package my.kata;
+
+import org.junit.jupiter.api.Test;
+
+class DemoTest {
+    @Test
+    void write_your_first_test() {
+
+    }
+}
